f9165e824040dfb5e992abd1ca88f0f40b456bfa
Chris Xing <Chris_Xing@epam.com>
2023-10-27 15:21:33 +0800

    initial discriminated union

--- Session.2/playground/discriminated.ts
+++ Session.2/playground/discriminated.ts
@@ -1,16 +1,4 @@
-interface Alert {
-  type: "alert";
-}
-
-interface Confirm {
-  type: "confirm";
-  confirmButtonMassage: string;
-}
-
-type Props = Alert | Confirm;
-
-function Modal(props: Props) {
-  const message = props.confirmButtonMassage.toUpperCase();
-
-  return null;
+interface Props {
+  type: "alert" | "confirm";
+  confirmButtonMassage?: string;
 }
